0b51a4e050cf76fbe6cff48075bead58685065db
guichagas-fullscript <guilherme.chagas@fullscript.com>
2019-11-19 22:58:53 -0500

    adding form

--- scripts/Main.js
+++ scripts/Main.js
@@ -2,6 +2,14 @@
 
 var collection_name = 'streets'
 
+var select_mun = document.getElementById("municipality");
+var select_str = document.getElementById("street");
+var select_fro = document.getElementById("from");
+var select_to = document.getElementById("to");
+
+var query_data = [];
+
+
 /**
  * Initializes the WinterWalk app.
  */
@@ -15,52 +23,12 @@ function WinterWalk() {
   };
 
   var that = this;
+  //var select = that.templates["municipality"];
 
   firebase.auth().signInAnonymously().then(function() {
     
     that.initRouter();
-
-    
-   // // test all streets
-   // that.getAllRestaurants()
-
-   var filtered_data = that.getFilteredRestaurants(that.filters)
-
-   filtered_data.then(
-       function(querySnapshot) {
-         querySnapshot.forEach(function(doc) {
-             console.log('filtered')
-             console.log(doc.ROAD_NAME_FULL)
-             console.log(doc.COORD)
-         });
-     })
-
-    // // query one restaurant
-    // var filtered_doc = that.getRestaurant("00T6XmnXpLUtG5MQKvl0")
-
-    // filtered_doc.then(function(result){
-
-    //   console.log('before')
-    //   console.log(result.id);
-    //   console.log(result.ROAD_NAME_FULL);
-
-    // });
-
-
-    // query filtered
-
-
-    // // add data
-    // var rest_data = {
-    //     name: 'Gui',
-    //     category: 'test',
-    //     price: '9',
-    //     city: 'Porto Alegre'
-    // };
-
-    // // add data
-    // //  var collection = firebase.firestore().collection('restaurants');
-    // //  return collection.add(rest_data);
+    that.initTemplates();
 
     }).catch(function(err) {
       console.log(err);
@@ -78,21 +46,99 @@ WinterWalk.prototype.initRouter = function() {
     .on({
       '/': function() {
         //that.updateQuery(that.filters);
+        that.getFilteredRestaurants(that.filters);
       }
     })
     .resolve();
 
-  firebase
-    .firestore()
-    .collection('streets')
-    .limit(1)
-    .onSnapshot(function(snapshot) {
-      // if (snapshot.empty) {
-      //   that.router.navigate('/setup');
-      // }
-    });
+  // firebase
+  //   .firestore()
+  //   .collection('streets')
+  //   .limit(1)
+  //   .onSnapshot(function(snapshot) {
+  //     // if (snapshot.empty) {
+  //     //   that.router.navigate('/setup');
+  //     // }
+  //   });
+};
+
+WinterWalk.prototype.initTemplates = function() {
+  this.templates = {};
+
+  var that = this;
+  document.querySelectorAll('.template').forEach(function(el) {
+    that.templates[el.getAttribute('id')] = el;
+  });
 };
 
 window.onload = function() {
   window.app = new WinterWalk();
+
+  // read initial streets
+  var filters_temp = {
+    municipality: 'Old Ottawa',
+    street: '',
+    from: '',
+    to: ''
+  };
+
+  var filtered_data = this.WinterWalk.prototype.getFilteredRestaurants(filters_temp)
+
+  var temp_municipality = [];
+  var temp_street = [];
+  var temp_from = [];
+  var temp_to = [];
+
+  filtered_data.then(
+        function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+              query_data.push( doc );
+
+              // add municipality
+              if ( !(temp_municipality.includes(doc.MUNICIPALITY)) ){
+                // add to dropdown list
+                temp_municipality.push( doc.MUNICIPALITY )
+                select_mun.options[select_mun.options.length] = new Option(doc.MUNICIPALITY, doc.MUNICIPALITY);
+              }
+
+              // add street
+              if ( !(temp_street.includes(doc.ROAD_NAME_FULL)) ){
+                // add to dropdown list
+                temp_street.push( doc.ROAD_NAME_FULL )
+                select_str.options[select_str.options.length] = new Option(doc.ROAD_NAME_FULL, doc.ROAD_NAME_FULL);
+              }
+
+              // add from
+              if ( !(temp_from.includes(doc.FROM_ROAD_NAME)) ){
+                // add to dropdown list
+                temp_from.push( doc.FROM_RD_NAME )
+                select_fro.options[select_fro.options.length] = new Option(doc.FROM_RD_NAME, doc.FROM_RD_NAME);
+              }
+
+              // add to
+              if ( !(temp_to.includes(doc.TO_ROAD_NAME)) ){
+                // add to dropdown list
+                temp_to.push( doc.TO_RD_NAME )
+                select_to.options[select_to.options.length] = new Option(doc.TO_RD_NAME, doc.TO_RD_NAME);
+              }
+
+          });
+        }
+  )
+  
+  // get unique municipality
+  
+
+  query_data.forEach(function(element) {
+    
+    
+  });
+  
+  
+    
+  
+  // // add data
+    // //  var collection = firebase.firestore().collection('restaurants');
+    // //  return collection.add(rest_data);
+
 };
